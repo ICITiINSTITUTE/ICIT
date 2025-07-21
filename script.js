8a3e75ec776a3c924a3cb04131e6503a2ccca4cd
ICITiINSTITUTE <icitcomputer50@gmail.com>
2025-07-21 22:13:39 +0530

    add fils

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function sayHello() {
-  alert('Hello from ICIT Computer!');
-}
+const userName = prompt("Enter the student name");
+
+    if (userName) {
+      document.getElementById("ak").innerText = "Welcome to ICIT INSTITUTE online registration from, " + userName + "!";
+    }
