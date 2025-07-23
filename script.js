f8afa722eb249ae6c6e2c478cfb991f82abaef87
ICITiINSTITUTE <icitcomputer50@gmail.com>
2025-07-23 21:22:22 +0530

    add files

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const userName = prompt("Enter the student name");
-
-    if (userName) {
-      document.getElementById("ak").innerText = "Welcome to ICIT INSTITUTE online registration from, " + userName + "!";
-    }
+ window.onload = function () {
+      let userName = prompt("Please enter the student name:");
+      if (userName) {
+        document.getElementById("ak").innerText =
+          "Welcome to ICIT Institute, " + userName + "!";
+      } else {
+        document.getElementById("ak").innerText =
+          "Welcome to ICIT Institute!";
+      }
+    };
